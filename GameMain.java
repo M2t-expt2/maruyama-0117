6d65837683abb34cea87f16bb39fdd31fa8a99b1
expt2 <m2tanaka.expt2@gmail.com>
2024-01-17 11:36:16 +0900

    first commit

---
+++ GameMain.java
@@ -0,0 +1,16 @@
+public class GameMain {
+  public static void main(String[] args) {
+    int playerHand;
+    // 課題1 文章の表示
+
+    // 課題2 Playerの手の決定とその表示
+    // HandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+
+    // 課題3 Computerの手の決定とその表示
+    // ComputerクラスのdecidesComputerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+
+    // 課題4 勝敗判定とその表示
+    // 課題2,3において実装したPlayerおよびComputerの手の表示はVictoryOrDefeatクラスに移動する
+
+  }
+}
